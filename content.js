25b4440c3d67da5e81fda0d76b492b383076b03d
unknown <admin@k2.zse.pl>
2021-11-18 12:28:44 +0100

    popup first stage

--- content.js
+++ content.js
@@ -1,20 +1,6 @@
 let isLoaded = false
 let pageViews = null
 
-// async function waitForLoad() {
-//     if(document.querySelector("span[data-testid='page-view-text']") != null) {
-//         console.log("page loaded!")
-//         isLoaded = true
-//     } else {
-//         console.log("page not loaded yet")
-//         waitForLoad();
-//     }
-//     await new Promise(r => setTimeout(r, 100));
-// }
-
-// while (!isLoaded) {
-//     await waitForLoad();
-// }
 
 if (document.URL.includes("d/oferta/")){
     waitForElementToDisplay("span[data-testid='page-view-text']",1000,9000);
@@ -42,10 +28,23 @@ function waitForElementToDisplay(selector, checkFrequencyInMs, timeoutInMs) {
 
 function callback() {
   console.log("start");
+
+  function showPopup(){
+    $('.hover_bkgr_fricc').show();
+    console.log("click")
+  }
   
-    notesBtn = '<i style="margin-right: 1em; height: 1em; width: 1em" class="far fa-comments"></i>';
+    notesBtn = '<i onclick="document.querySelector(\'.hover_bkgr_fricc\').classList.remove(\'hidden\');" style="margin-right: 1em; height: 1em; width: 1em" class="far fa-comments"></i>';
     $(".css-sg1fy9 div").prepend(notesBtn);
+
+
 
     let pageViews = $("span[data-testid='page-view-text']").text();
     console.log(pageViews);
+
+    let popup = '<div class="hover_bkgr_fricc hidden"><span class="helper"></span><div><div onclick="document.querySelector(\'.hover_bkgr_fricc\').classList.add(\'hidden\');" class="popupCloseButton">&times;</div><p>Add any HTML content<br />inside the popup box!</p></div></div>'
+    $("body").append(popup);
+
+
+
 }
